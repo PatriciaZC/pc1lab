f4f83b48a8c3368d701177da6aa429a92e028ba9
Patricia Zambrano <pzambranocarpio@gmail.com>
2020-10-19 14:31:55 -0500

    Startup

--- Startup.cs
+++ Startup.cs
@@ -5,9 +5,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using pc1lab.Data;
 
 namespace pc1lab
 {
@@ -23,6 +25,9 @@ namespace pc1lab
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<DatabaseContext>(options =>
+            options.UseNpgsql(
+            Configuration.GetConnectionString("postgress-db")));
             services.AddControllersWithViews();
         }
 
